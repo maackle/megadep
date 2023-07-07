4debde239da94f083488b3250c7eef62656d44d0
Michael Dougherty <maackle.d@gmail.com>
2023-07-06 18:13:15 -0700

    Megadep struct

--- examples/alphabeta.rs
+++ examples/alphabeta.rs
@@ -1,13 +1,8 @@
 use std::path::PathBuf;
 
 fn main() {
-    let deps = vec![
-        "./target/debug/deps".into(),
-        // PathBuf::from("./crates/alpha"),
-        // PathBuf::from("./crates/beta"),
-    ];
-    println!("this works.");
-    megadep::process(PathBuf::from("./crates/alpha/src/lib.rs"), deps.clone());
-    println!("this fails, because of the dependency on `alpha`.");
-    megadep::process(PathBuf::from("./crates/beta/src/lib.rs"), deps.clone());
+    let m = megadep::Megadep {
+        deps_dir: "./target/debug/deps".to_string(),
+    };
+    m.process(PathBuf::from("./crates/beta/src/lib.rs"));
 }
--- src/lib.rs
+++ src/lib.rs
@@ -22,85 +22,95 @@ use rustc_span::edition::Edition;
 // use rustc_hash::{FxHashMap, FxHashSet};
 // use rustc_span::source_map;
 
-pub fn process(path: PathBuf, crate_deps: Vec<PathBuf>) {
-    holochain_trace::test_run().ok();
-    let out = process::Command::new("rustc")
-        .arg("--print=sysroot")
-        .current_dir(".")
-        .output()
-        .unwrap();
-    // let sysroot = str::from_utf8(&out.stdout).unwrap().trim();
-    let mut externs = BTreeMap::new();
-    let location = ExternLocation::FoundInLibrarySearchDirectories;
-    externs.insert(
-        "alpha".to_string(),
-        ExternEntry {
-            location: ExternLocation::FoundInLibrarySearchDirectories,
-            is_private_dep: false,
-            add_prelude: false,
-            nounused_dep: false,
-            force: false,
-        },
-    );
-    // externs.insert(
-    //     "beta".to_string(),
-    //     ExternEntry {
-    //         location,
-    //         is_private_dep: false,
-    //         add_prelude: false,
-    //         nounused_dep: false,
-    //         force: false,
-    //     },
-    // );
-
-    let search_paths: Vec<_> = crate_deps
-        .into_iter()
-        .map(|p| {
-            let p = format!("{}", p.display());
-            SearchPath::from_cli_opt(&EarlyErrorHandler::new(Default::default()), &p)
-        })
-        .collect();
-
-    dbg!(&search_paths[0].files.len());
-    dbg!(&externs);
-
-    let opts0 = Options {
-        // maybe_sysroot: Some(path::PathBuf::from(sysroot)),
-        search_paths,
-        externs: rustc_session::config::Externs::new(externs),
-        edition: Edition::Edition2021,
-        crate_types: vec![default_lib_output()],
-        ..Options::default()
-    };
-
-    let opts =
-        config::parse_opts("--edition 2021 --crate-type lib -L target/debug/deps --extern alpha");
-
-    let config = config::config(path, opts);
-
-    rustc_interface::run_compiler(config, |compiler| {
-        compiler.enter(|queries| {
-            // Parse the program and print the syntax tree.
-            // let parse = queries.parse().unwrap().get_mut().clone();
-            // println!("{parse:#?}");
-
-            // Analyze the program and inspect the types of definitions.
-            queries.global_ctxt().unwrap().enter(|tcx| {
-                let hir = tcx.hir();
-                for id in hir.items() {
-                    // dbg!(&id);
-                    let item = hir.item(id);
-                    dbg!(&item);
-                    match item.kind {
-                        rustc_hir::ItemKind::Static(_, _, _) | rustc_hir::ItemKind::Fn(_, _, _) => {
-                            let name = item.ident;
-                            let ty = tcx.type_of(item.hir_id().owner.def_id);
-                            println!("{name:?}:\t{ty:?}")
-                        }
-                        _ => (),
-                    }
-                }
-            })
-        });
-    });
+pub struct Megadep {
+    pub deps_dir: String,
 }
+
+impl Megadep {
+    pub fn process(&self, path: PathBuf) {
+        holochain_trace::test_run().ok();
+
+        let opts = config::parse_opts(&format!(
+            "--edition 2021 --crate-type lib -L {} --extern alpha",
+            self.deps_dir
+        ));
+
+        let config = config::config(path, opts);
+
+        rustc_interface::run_compiler(config, |compiler| {
+            compiler.enter(|queries| {
+                // Parse the program and print the syntax tree.
+                // let parse = queries.parse().unwrap().get_mut().clone();
+                // println!("{parse:#?}");
+
+                // Analyze the program and inspect the types of definitions.
+                queries.global_ctxt().unwrap().enter(|tcx| {
+                    let hir = tcx.hir();
+                    for id in hir.items() {
+                        // dbg!(&id);
+                        let item = hir.item(id);
+                        dbg!(&item.kind, &item.ident);
+                        match item.kind {
+                            rustc_hir::ItemKind::Static(_, _, _)
+                            | rustc_hir::ItemKind::Fn(_, _, _) => {
+                                let name = item.ident;
+                                let ty = tcx.type_of(item.hir_id().owner.def_id);
+                                println!("{name:?}:\t{ty:?}")
+                            }
+                            _ => (),
+                        }
+                    }
+                })
+            });
+        });
+    }
+}
+/*
+
+   let out = process::Command::new("rustc")
+       .arg("--print=sysroot")
+       .current_dir(".")
+       .output()
+       .unwrap();
+   let sysroot = str::from_utf8(&out.stdout).unwrap().trim();
+   let mut externs = BTreeMap::new();
+   let location = ExternLocation::FoundInLibrarySearchDirectories;
+   externs.insert(
+       "alpha".to_string(),
+       ExternEntry {
+           location: ExternLocation::FoundInLibrarySearchDirectories,
+           is_private_dep: false,
+           add_prelude: false,
+           nounused_dep: false,
+           force: false,
+       },
+   );
+   // externs.insert(
+   //     "beta".to_string(),
+   //     ExternEntry {
+   //         location,
+   //         is_private_dep: false,
+   //         add_prelude: false,
+   //         nounused_dep: false,
+   //         force: false,
+   //     },
+   // );
+
+   let search_paths = vec![SearchPath::from_cli_opt(
+       &EarlyErrorHandler::new(Default::default()),
+       &deps_dir.to_str().expect("bad deps dir"),
+   )];
+
+   dbg!(&search_paths[0].files.len());
+   dbg!(&externs);
+
+   let opts0 = Options {
+       // maybe_sysroot: Some(path::PathBuf::from(sysroot)),
+       search_paths,
+       externs: rustc_session::config::Externs::new(externs),
+       edition: Edition::Edition2021,
+       crate_types: vec![default_lib_output()],
+       ..Options::default()
+   };
+
+*/
